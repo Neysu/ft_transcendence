b3eb669f7c1d03aba982c6be27959b5683fdadc6
Elliot <elliot.gibeaux@proton.me>
2026-02-02 14:07:09 +0100

    added schema for round + games, modified changed certs.sh for better certs generation, added volumes for key persistance and modified default.conf for the $HOST

--- back.Dockerfile
+++ back.Dockerfile
@@ -1,24 +1,14 @@
-FROM oven/bun:debian AS base
+FROM oven/bun:latest
+
 WORKDIR /app
 
-FROM base AS install
-RUN mkdir -p /temp/dev
-COPY backend/package.json backend/bun.lock /temp/dev/
-RUN cd /temp/dev && bun install --frozen-lockfile
+COPY ./backend/package.json .
+RUN bun install
 
-RUN mkdir -p /temp/prod
-COPY backend/package.json backend/bun.lock /temp/prod/
-RUN cd /temp/prod && bun install --frozen-lockfile --production
-
-FROM base AS prerelease
-COPY --from=install /temp/dev/node_modules node_modules
 COPY backend/ .
 
-FROM base AS release
-COPY --from=install /temp/prod/node_modules node_modules
-COPY --from=prerelease /app/ .
+EXPOSE 3000
 
-EXPOSE 3000/tcp
-RUN bun prisma migrate dev
+RUN bun prisma migrate dev --schema="./prisma/schema.prisma" --name="test"
 RUN bun prisma generate
 CMD ["bun", "run", "index.ts"]
--- certs.sh
+++ certs.sh
@@ -1,25 +1,25 @@
 #!/bin/sh
 
 certs_dir="/etc/nginx/certs"
-if [ ! -d "$certs_dir" ]; then
-	mkdir "$certs_dir"
-fi
+mkdir -p "$certs_dir"
 
-if [ ! -f "$certs_dir/cert.pem" ] || [ ! -f "$certs_dir/key.pem" ]; then
-	echo "Generating self-signed SSL certs for front"
+# Generate self-signed certs if they don't exist
+if [ ! -f "$certs_dir/certs.crt" ] || [ ! -f "$certs_dir/keys.key" ]; then
+    echo "Generating self-signed SSL certs for Nginx"
+    : "${HOST:=ft_trans.localhost}"  # default if HOST not set
 
-	openssl req -x509 -nodes -days 365 -newkey rsa:2048 \
-        -keyout "$certs_dir/keys.pem" \
-        -out "$certs_dir/certs.pem" \
+    openssl req -x509 -nodes -days 365 -newkey rsa:2048 \
+        -keyout "$certs_dir/keys.key" \
+        -out "$certs_dir/certs.crt" \
         -subj "/CN=${HOST}" \
         -addext "subjectAltName=DNS:${HOST},DNS:localhost,IP:127.0.0.1"
 
-    chmod 644 "$certs_dir/certs.pem"
-    chmod 600 "$certs_dir/keys.pem"
+    chmod 644 "$certs_dir/certs.crt"
+    chmod 600 "$certs_dir/keys.key"
 
-	echo "SSL certs generated successfully"
+    echo "SSL certs generated successfully"
 else
-	echo "SSL certs already exist skipping"
+    echo "SSL certs already exist, skipping"
 fi
 
 exec "$@"
